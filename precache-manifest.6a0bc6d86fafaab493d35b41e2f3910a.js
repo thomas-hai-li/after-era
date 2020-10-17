8ee368f2842efbd0cf71d2bf6e2bd901e1c37cf7
Thomas H. Li <34053587+thomas-hai-li@users.noreply.github.com>
2020-10-17 04:39:27 -0400

    Updates

---
+++ precache-manifest.6a0bc6d86fafaab493d35b41e2f3910a.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c01932347c1cac16066606b8e879c15",
+    "url": "/after-era/index.html"
+  },
+  {
+    "revision": "eb9afb7db2c7d7f2de8d",
+    "url": "/after-era/static/css/main.015e594f.chunk.css"
+  },
+  {
+    "revision": "c75e9072593f69e6b026",
+    "url": "/after-era/static/js/2.9aea9a2a.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/after-era/static/js/2.9aea9a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb9afb7db2c7d7f2de8d",
+    "url": "/after-era/static/js/main.6c1e5851.chunk.js"
+  },
+  {
+    "revision": "7c04de2354e19c95a457",
+    "url": "/after-era/static/js/runtime-main.104b4a8b.js"
+  },
+  {
+    "revision": "1a5ec38551a4a715ae5653bf6e49efd0",
+    "url": "/after-era/static/media/1.1a5ec385.jpg"
+  },
+  {
+    "revision": "3d1f6aa866ca1e4ff51456ca59c1faf4",
+    "url": "/after-era/static/media/1.3d1f6aa8.gif"
+  },
+  {
+    "revision": "76aa74775df83eec60d4544e78f4ca0f",
+    "url": "/after-era/static/media/1.76aa7477.jpg"
+  },
+  {
+    "revision": "9d0abde93fc1ea56369c9d8ca430717b",
+    "url": "/after-era/static/media/1.9d0abde9.png"
+  },
+  {
+    "revision": "b30841c89272c1bf27ec07a3211f7ebd",
+    "url": "/after-era/static/media/1.b30841c8.jpg"
+  },
+  {
+    "revision": "be2b2a7dedc552395b3b1cb07b354799",
+    "url": "/after-era/static/media/1.be2b2a7d.png"
+  },
+  {
+    "revision": "4a0d73b12398e3604ba6ac6c48d4802f",
+    "url": "/after-era/static/media/2.4a0d73b1.gif"
+  },
+  {
+    "revision": "56ce00223d024f632c7e1a518f832c2a",
+    "url": "/after-era/static/media/2.56ce0022.gif"
+  },
+  {
+    "revision": "6cba11db13ffae640506f84a40a71c42",
+    "url": "/after-era/static/media/2.6cba11db.gif"
+  },
+  {
+    "revision": "75cca76e636b473daba6b77e2187fa89",
+    "url": "/after-era/static/media/2.75cca76e.gif"
+  },
+  {
+    "revision": "7c92135d50d6eb75c97bee95543a46b9",
+    "url": "/after-era/static/media/2.7c92135d.gif"
+  },
+  {
+    "revision": "952025c9cd482cf5a21db169ff3e9f54",
+    "url": "/after-era/static/media/2.952025c9.jpg"
+  },
+  {
+    "revision": "90f28bba08d2aa3d872d190a2f53fe3a",
+    "url": "/after-era/static/media/3.90f28bba.gif"
+  },
+  {
+    "revision": "bf7421240b7f9b3de838bfc25a203dbe",
+    "url": "/after-era/static/media/3.bf742124.gif"
+  },
+  {
+    "revision": "cfe649b03ad0c62ae1044d56d2880313",
+    "url": "/after-era/static/media/3.cfe649b0.jpg"
+  },
+  {
+    "revision": "eff99afb36f9267d9d1d9bf297d14ca8",
+    "url": "/after-era/static/media/3.eff99afb.gif"
+  },
+  {
+    "revision": "f474ab352b1709c7959c134f463fb4c5",
+    "url": "/after-era/static/media/3.f474ab35.gif"
+  },
+  {
+    "revision": "62e9c296b8f10c285d135efddd9e8d89",
+    "url": "/after-era/static/media/4.62e9c296.webp"
+  },
+  {
+    "revision": "6bbae71ab567791110f6c8c43263031e",
+    "url": "/after-era/static/media/4.6bbae71a.gif"
+  },
+  {
+    "revision": "97b25367e38a548c75395427799698c6",
+    "url": "/after-era/static/media/4.97b25367.jpg"
+  },
+  {
+    "revision": "985e61cf06d508649eba5f460158be6f",
+    "url": "/after-era/static/media/5.985e61cf.gif"
+  },
+  {
+    "revision": "c97da16b01c648758f20557fd42f94cd",
+    "url": "/after-era/static/media/5.c97da16b.gif"
+  },
+  {
+    "revision": "7a8e6fe866edbbda016b4084b9310d37",
+    "url": "/after-era/static/media/6.7a8e6fe8.gif"
+  },
+  {
+    "revision": "3530ba6863f5c37e16c15b7908e22765",
+    "url": "/after-era/static/media/Crayawn-v58y.3530ba68.ttf"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/after-era/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "9f9dc99f5285da9ef9c33145f06e203c",
+    "url": "/after-era/static/media/logo1.9f9dc99f.png"
+  },
+  {
+    "revision": "04ce36ef9c197a671bcc8be79b698a5e",
+    "url": "/after-era/static/media/thumbnail.04ce36ef.webp"
+  },
+  {
+    "revision": "36ae413e4fb3bd8b21925a9e8e41707d",
+    "url": "/after-era/static/media/thumbnail.36ae413e.gif"
+  },
+  {
+    "revision": "56ce00223d024f632c7e1a518f832c2a",
+    "url": "/after-era/static/media/thumbnail.56ce0022.gif"
+  },
+  {
+    "revision": "d0fcc901b95103e7b9e25fe80e584bf6",
+    "url": "/after-era/static/media/thumbnail.d0fcc901.jpg"
+  }
+]);
